f5b89d002e6e01b8e7b080ad50bc36a0ee9fba53
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-02-19 12:33:48 +0100

    How to use hook useEffect

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,35 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 
-const List = () => {
-    return (
-        <div className={styles.list}>
-            <header className={styles.header}>
-                <h2 className={styles.title}>Things to do<span>soon</span></h2>
-            </header>
-            <p className={styles.description}>Interesting things I want to check out</p>
-            <section className={styles.columns}>
-                <Column icon="book" title="Books" />
-                <Column icon="gamepad" title="Movies" />
-                <Column icon="film" title="Games" />
-            </section>
-        </div>    
-    );
-  };
+import { useState, useEffect } from 'react';
 
-  export default List;
+const List = () => {
+
+  const [columns, setColumns] = useState([
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setColumns([...columns, { id: 4, title: 'Test column'}]);
+    }, 2000);
+
+  }, []);
+
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Things to do<span>soon</span></h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+        {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+      </section>
+    </div>    
+  );
+};
+
+export default List;
