e591b4c8eabe22ceed55cc869e1c4ddac6af1896
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-02-19 02:43:46 +0100

    Add components: Button, List, Column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,5 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
     return (
@@ -8,15 +9,9 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <article>
-                    <h2>Books</h2>
-                </article>
-                <article>
-                    <h2>Movies</h2>
-                </article>
-                <article>
-                    <h2>Games</h2>
-                </article>
+                <Column icon="book" title="Books" />
+                <Column icon="gamepad" title="Movies" />
+                <Column icon="film" title="Games" />
             </section>
         </div>    
     );
