bffd82f775b56bd1ccbce836343afd2a7a05f0d9
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-03-12 14:23:57 +0100

    Add Pagetitle component and Error page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Hero from './components/Hero/Hero'
-import SearchForm from './components/SearchForm/SearchForm'
-import List from './components/List/List'
+import Home from './components/Home/Home'
 import Container from './components/Container/Container'
 import NavBar from './components/NavBar/NavBar'
 import About from './components/About/About'
 import Favorite from './components/Favorite/Favorite'
-
+import NotFound from './components/NotFound/NotFound'
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
-        <About />
-        <Favorite />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/favorite" element={<Favorite />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
       </Container>
     </main>
 
